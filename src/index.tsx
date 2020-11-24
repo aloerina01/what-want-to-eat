72277663eadef9d1ffe3f3ab32a94e0ca5e8d16c
aloerina01 <4443321+aloerina01@users.noreply.github.com>
2020-11-24 21:05:29 +0900

    Add firebase config

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,9 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import { App } from './App';
+import { initialize } from './firebase/initialize';
 
+initialize();
 const rootElement = document.getElementById('root');
 render(
   <BrowserRouter>
