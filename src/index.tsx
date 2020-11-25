d0beabe1d7d990ef8a233518903e6fb49dbd7fb2
aloerina01 <4443321+aloerina01@users.noreply.github.com>
2020-11-25 11:55:42 +0900

    Implement masterFoodItemsRepo

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,7 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import { App } from './App';
-import { initialize } from './firebase/initialize';
 
-initialize();
 const rootElement = document.getElementById('root');
 render(
   <BrowserRouter>
---
+++ src/repositories/masterFoodItemsRepo.ts
@@ -0,0 +1,27 @@
+import firebase from 'firebase/app';
+import { DB } from '../firebase';
+import { IFoodItem } from '../models/IFoodItem';
+
+const masterFoodItemsRef = DB.collection('masterFoodItems');
+
+export interface IMasterFoodItemsRepo {
+  getAll: () => Promise<IFoodItem[]>;
+  add: (foodItem: IFoodItem) => Promise<void>;
+}
+
+export const masterFoodItemsRepo: IMasterFoodItemsRepo = {
+  getAll: async () => {
+    const snapshot = await masterFoodItemsRef.orderBy('order', 'asc').get();
+    return snapshot.docs.map((queryDocumentSnapshot) => {
+      return {
+        id: queryDocumentSnapshot.id,
+        name: queryDocumentSnapshot.data().name,
+        icon: queryDocumentSnapshot.data().icon,
+        image: queryDocumentSnapshot.data().image,
+      };
+    });
+  },
+  add: (foodItem) => {
+    return masterFoodItemsRef.doc().set(foodItem);
+  },
+};
