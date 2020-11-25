da3470a23278e61edc82bd2f78eeb9d018cfbf65
aloerina01 <4443321+aloerina01@users.noreply.github.com>
2020-11-25 21:13:28 +0900

    Fix bug

--- src/liff/index.ts
+++ src/liff/index.ts
@@ -1,18 +1,11 @@
 import liff from '@line/liff';
 
 liff.init({ liffId: '1654302088-rB1Ab9ad' }, () => {
-  if (process.env.NODE_ENV === 'development') {
-    console.log('liff loggedIn:', liff.isLoggedIn());
-    console.log('liff decodedIDToken:', liff.getDecodedIDToken());
+  if (!liff.isLoggedIn()) {
+    liff.login();
   }
-  // if (!liff.isLoggedIn()) {
-  //   liff.login();
-  // }
 });
 
 export const getDecodedIDToken = () => {
-  if (process.env.NODE_ENV === 'development') {
-    return { sub: 'dummy_usrId' };
-  }
   return liff.getDecodedIDToken();
 };
